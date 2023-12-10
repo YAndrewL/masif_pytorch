0a36ce82098d2b44e5a63b9c8f4b36fec8d4f659
Yufan Liu <liuy0n@vsc509-03-l.ibex.kaust.edu.sa>
2023-12-10 12:11:20 +0300

    update hydro compute

--- data_prepare.py
+++ data_prepare.py
@@ -6,9 +6,12 @@
 @Desc   :  Generate features and path
 '''
 import os
-from preprocessing.surface import protonate, extractPDB, generate_surface 
+from preprocessing.computeSurface import protonate, extractPDB, generate_surface 
+from preprocessing.computeCharge import generate_charge
+from preprocessing.computeHydro import generate_hydrophabicity
 from typing import List
 from tqdm import tqdm
+import time
 # for data preprocessing
 # PDB -> precompute features and shortest path
 # todo surface use pymol; shortest path use 
@@ -24,7 +27,7 @@ class DataPrepare(object):
     def run_protonate(self, args, data):
         input_file = os.path.join(args.raw_path, data.split('_')[0] + '.pdb')
         output_file = os.path.join(args.processed_path, data, 'raw.pdb') 
-        protonate(input_file, output_file)
+        protonate(args, input_file, output_file)
 
     def run_extractPDB(self, args, data, chain1, chain2):
         input_file = os.path.join(args.processed_path, data, 'raw.pdb')
@@ -33,13 +36,18 @@ class DataPrepare(object):
         extractPDB(input_file, out_chain1, chain1)
         extractPDB(input_file, out_chain2, chain2)
 
-    def run_surface(self, args, data, num=None):
+    def run_surface(self, args, data, num):
         input_file = os.path.join(args.processed_path, data, f'p{num}.pdb')
         output_file = os.path.join(args.processed_path, data, f'p{num}.xyzrn')
-        return generate_surface(args, input_file, output_file)
+        return generate_surface(args, input_file, output_file, cache=False)
 
-    def run_features(self):
-        pass
+    def run_charge(self, args, data, vertices, names, num):
+        input_file = os.path.join(args.processed_path, data, f'p{num}.pdb')
+        return generate_charge(input_file, vertices, names)
+
+    def run_hydro(self, args, data, names, num):
+        input_file = os.path.join(args.processed_path, data, f'p{num}.pdb')
+        return generate_hydrophabicity(input_file, names)
 
 
     def __call__(self):
@@ -51,22 +59,42 @@ class DataPrepare(object):
         if not os.path.exists(processed_path):
             os.mkdir(processed_path) 
         
+        print("Start processing data...")
         for data in tqdm(data_list):
+            start_time = time.time()
             # todo consider multi-processing in the future
             # data is like 1A0G_A_B
             pdb, chain1, chain2 = data.split('_')
             if not os.path.exists(os.path.join(processed_path, data)):
                 os.mkdir(os.path.join(processed_path, data))
 
-            # Stage 1. generate surface 
+            # Stage I. generate surface 
             # 1. protonate
             self.run_protonate(args, data)
+            print(f'running time for protonating: {time.time() - start_time}')
+
             # 2. extract PDB, all process after then should be executed in processed path
             self.run_extractPDB(args, data, chain1, chain2)
+            print(f'running time for extracting PDB: {time.time() - start_time}')
+
+            
             # process 2 chains separately from then on
             for num in range(2):
                 num += 1
                 # 3. generate surface, including non-standard amino acids
                 vertices, faces, normals, names, areas = self.run_surface(args, data, num)
+                print(f'running time for generating surface of component{num}: {time.time() - start_time}')
 
-                # Stage 2. generatue features
+                # Stage II. generatue features
+                # 1. generate electrons +/- donor and acceptor
+                charge = self.run_charge(args, data, vertices, names, num)
+                print(f'running time for computing charge of component{num}: {time.time() - start_time}')
+
+                # 2. generate logp
+                logp = self.run_hydro(args, data, names, num)
+                print(f'running time for computing hydrophabicity of component{num}: {time.time() - start_time}')
+                # print(len(logp) == len(vertices))
+                # print(len(charge) == len(vertices))
+
+                # 3. generate APBS feature
+                
--- preprocessing/computeHydro.py
+++ preprocessing/computeHydro.py
@@ -8,21 +8,36 @@
 # todo THIS used too many loops, but I'm lazy to modify this now, and compute them in advance, idc
 import Bio.PDB as biopdb
 import numpy as np
+import os
+from rdkit import Chem
+from rdkit.Chem import AllChem
 
-
-def generate_hydro(infilename):
+def generate_hydrophabicity(infilename, names):
     # split PDB file
-    hydro = 
+    #hydro = np.zeros(len(names))  # just a length, no matter who is passed
     parser = biopdb.PDBParser(QUIET=True)
-    strtucture = parser.get_structure()
-    residues = biopdb.Selection.unfold_entities()
+    strtucture = parser.get_structure(infilename, infilename)
+    residues = biopdb.Selection.unfold_entities(strtucture, 'R')
+    hydro = dict()
     for residue in residues:
-
-
-parser = biopdb.PDBParser(QUIET=True)
-struct = parser.get_structure('test', 'chainI.pdb')
-model = biopdb.Selection.unfold_entities(struct, 'R')
-print(model.het)
-# Select residues to extract and build new structure
-structBuild = biopdb.StructureBuilder.StructureBuilder()
-structBuild.init_structure("output")
+        res_id = residue.get_id()
+        if res_id[0] != ' ':
+            res_id = (' ', res_id[1], res_id[2])
+        io = biopdb.PDBIO()
+        io.set_structure(residue)
+        tmp = ''.join(infilename.split('.')[:-1]) + '.tmp.' + str(res_id)
+        io.save(tmp)
+        mol = Chem.rdmolfiles.MolFromPDBFile(tmp)
+        logp_mol = AllChem.CalcCrippenDescriptors(mol)[0]  
+        # here the first is logp, then normalize to -1 ~ 1
+        os.remove(tmp)
+        hydro[res_id] = logp_mol
+    # assign to each vertex
+    logp = np.zeros(len(names))
+    for idx, name in enumerate(names):
+        fields = name.split('_')
+        if fields[2] == "x":
+            fields[2] = " "
+        match = (" ", int(fields[1]), fields[2])
+        logp[idx] = hydro[match]
+    return logp
